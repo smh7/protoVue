e72bc97dd64c9cf745d3be96f3bd5e8ad9516f4b
Steven Howard <steven@smhoward.net>
2020-09-16 11:03:31 -0400

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,75 @@
+<template>
+<b-container>
+  <b-row align-v="center">
+    <b-col md="3">
+      <b-card
+      title="This is a Card"
+      img-src="https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y"
+      img-alt="report-info"
+      img-top
+      tag="lending"
+      style="max-width:20 rem"
+      class="mb-2"
+      >
+        <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptas dolorum saepe, beatae ut quidem debitis, recusandae ratione culpa suscipit, sapiente unde facere. Ad, eos quae? Iure corrupti sequi nesciunt?</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+
+        <b-col md="3">
+      <b-card
+      title="This is a Card"
+      img-src="https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y"
+      img-alt="report-info"
+      img-top
+      tag="lending"
+      style="max-width:20 rem"
+      class="mb-2"
+      >
+        <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptas dolorum saepe, beatae ut quidem debitis, recusandae ratione culpa suscipit, sapiente unde facere. Ad, eos quae? Iure corrupti sequi nesciunt?</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+
+        <b-col md="3">
+      <b-card
+      title="This is a Card"
+      img-src="https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y"
+      img-alt="report-info"
+      img-top
+      tag="lending"
+      style="max-width:20 rem"
+      class="mb-2"
+      >
+        <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptas dolorum saepe, beatae ut quidem debitis, recusandae ratione culpa suscipit, sapiente unde facere. Ad, eos quae? Iure corrupti sequi nesciunt?</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+
+        <b-col md="3">
+      <b-card
+      title="This is a Card"
+      img-src="https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y"
+      img-alt="report-info"
+      img-top
+      tag="lending"
+      style="max-width:20 rem"
+      class="mb-2"
+      >
+        <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptas dolorum saepe, beatae ut quidem debitis, recusandae ratione culpa suscipit, sapiente unde facere. Ad, eos quae? Iure corrupti sequi nesciunt?</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {
+  }
+};
+</script>
